afc03ad553aff31b741a4f7f36159ef99fb2fb9b
liutianhong <tianhong.liu@me2zen.com>
2018-06-20 17:55:24 +0800

    add

---
+++ meadowlark.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+app.get('/about', function(req, res){
+    res.type('text/plain');
+    res.send('heiheihei about');
+});
+// 定制404页面 
+app.use(function(req, res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+// 定制 500 页面
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+app.listen(app.get('port'), function(){
+    console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
+
+app.use(express.static(__dirname + '/client'));
